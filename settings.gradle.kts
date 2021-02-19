52b35460d34efde7e0bf4e9066375d38bd53559c
Konstant13 <emelyanov262019@yandex.ru>
2021-02-19 16:51:41 +0300

    добавил весь проект в новую ветку

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
 rootProject.name = "otuskotlin-marketplace"
 
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion apply false
+        kotlin("js") version kotlinVersion apply false
+        kotlin("multiplatform") version kotlinVersion apply false
+        kotlin("plugin.serialization") version kotlinVersion apply false
+    }
+}
+
+include("ok-comments-common-mp")
+//include("ok-comments-common-be")
+//include("ok-comments-frontend")
+//include("ok-comments-transport-mp")
+//include("ok-comments-mappers-mp")
