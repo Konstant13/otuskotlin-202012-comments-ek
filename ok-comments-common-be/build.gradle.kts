45373ab599f824476712917ef4f5ab917db2b416
Konstant13 <emelyanov262019@yandex.ru>
2021-02-20 16:05:48 +0300

    Добавил весь проект в новую ветку

---
+++ ok-comments-common-be/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = rootProject.group
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
